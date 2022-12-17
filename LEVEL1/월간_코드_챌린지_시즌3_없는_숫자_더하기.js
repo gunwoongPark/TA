5141bce89ddb107ceddcc85747ea7c671a0f3588
gunwoongPark <bung1438@gmail.com>
2022-12-18 02:23:01 +0900

    프로그래머스 레벨 1

--- LEVEL1/월간_코드_챌린지_시즌3_없는_숫자_더하기.js
+++ LEVEL1/월간_코드_챌린지_시즌3_없는_숫자_더하기.js
@@ -1,14 +1,17 @@
-function solution(n) {
+function solution(numbers) {
   let answer = 0;
+  let tenArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  for (let num = 1; num <= n; ++num) {
-    if (n % num === 1) {
-      answer = num;
-      break;
-    }
-  }
+  numbers.forEach((num) => {
+    tenArr.splice(
+      tenArr.findIndex((el) => el === num),
+      1
+    );
+  });
+
+  tenArr.forEach((el) => (answer += el));
 
   return answer;
 }
 
-console.log(solution(10));
+console.log(solution([1, 2, 3, 4, 6, 7, 8, 0]));
